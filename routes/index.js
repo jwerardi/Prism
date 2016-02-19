2ae495676b1eea1a34babb30654ef02e1d1230a5
Tyler Bainbridge <tyler_bainbridge@student.uml.edu>
2016-02-19 13:54:30 -0500

    error message login

--- routes/index.js
+++ routes/index.js
@@ -4,21 +4,27 @@ var Account = require('../models/account');
 var router = express.Router();
 
 
-
+//receives the index view along with request.user, the jade file will then decide which version of the home page to display depending
+//on if the user is currently authenticated.
 router.get('/', function (req, res) {
   res.render('index', { user : req.user });
 });
 
+//receives the register view which has a register form
 router.get('/register', function(req, res) {
   res.render('register', { });
 });
 
+//all of the register form information is then passed to a new Account model, and redirected to the index view, but this time will be prompted with
+//a view of their profile
 router.post('/register', function(req, res) {
+  //registers a new account with the following information complete from the form
   Account.register(new Account({ username : req.body.username, fullname: req.body.name, title : req.body.title, propic: req.body.propic}), req.body.password, function(err, account) {
     if (err) {
+      //if the username is not unique, let them know in "info"
       return res.render("register", {info: "Sorry. That username already exists. Try again."});
     }
-
+    //authenticate and redirect to the index and show the profile
     passport.authenticate('local')(req, res, function () {
       res.redirect('/');
     });
@@ -29,9 +35,36 @@ router.get('/login', function(req, res) {
   res.render('login', { user : req.user });
 });
 
-router.post('/login', passport.authenticate('local'), function(req, res) {
-  res.redirect('/');
+
+router.post('/login', function(req, res, next) {
+  passport.authenticate('local', function(err, user, info) {
+    if (err) {
+      return next(err); // will generate a 500 error
+    }
+    // Generate a JSON response reflecting authentication status
+    if (! user) {
+      return res.render("login", {info: "Sorry, your credentials are invalid."});
+    }
+    req.login(user, function(err){
+      if(err){
+        return next(err);
+      }
+      return res.redirect("/");
+    });
+  })(req, res, next);
 });
+
+/*
+//attempt to authenticate the user's credentials
+router.post('/login', passport.authenticate('local'), function(req, res) {
+  if (err) {
+    //if the authentication is not successfull
+    return res.render("login", {info: "Invalid Credentials"});
+  }else{
+    res.redirect('/');
+  }
+});
+*/
 
 router.get('/logout', function(req, res) {
   req.logout();
