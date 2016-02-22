23c5aba60ec22040e2f5a3aa69492654b97fad42
Tyler Bainbridge <tyler_bainbridge@student.uml.edu>
2016-02-22 16:37:34 -0500

    integrated alpha version of masonry image gallery

--- public/js/gallery.js
+++ public/js/gallery.js
@@ -2,15 +2,28 @@
  * Created by tyler on 2/21/16.
  */
 
+$( window ).load( function()
+{
+    $( '#list' ).masonry( {
+        itemSelector: '.item',
+        isFitWidth: true} );
+});
+
 //VERY simple way of adding the images to the profile
 $(document).ready(function () {
+
     var images ="";
+
     for(var i =0; i<currentUser.images.length;i++)
     {
         //img(src= " " + user.propic)
-        images += " <img src=\"" + currentUser.images[i] + "\" + style=\"width:600px;height:400px;\">";
+        images +=
+            "<div class=\"item\">" +
+                "<img src=\"" + currentUser.images[i] + "\"/>" +
+            "</div>";
     }
+    console.log(images);
     //adding photos to page on DOM
-    $("#photos").html(images);
+    $("#list").html(images);
 
 });
