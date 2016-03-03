0cf1fd1068301494ec04f6a7773144312f08dcd4
Tyler Bainbridge <tyler_bainbridge@student.uml.edu>
2016-03-02 21:23:59 -0500

    explore limit decreased

--- routes/index.js
+++ routes/index.js
@@ -152,7 +152,7 @@ router.get('/explore', function(req, res) {
   // Find "limit" random documents (defaults to array of 1)
 
   var filter = { images: { $exists: true, $not: {$size: 0}}, propic: {$exists: true, $ne: ''} };
-  Account.findRandom(filter, {}, {skip: 0, limit: 30}, function(err, results) {
+  Account.findRandom(filter, {}, {skip: 0, limit: 4}, function(err, results) {
     if (!err) {
       console.log(results[1].username); // 5 elements
       return res.render('explore', {users: results, currentuser: req.user});
