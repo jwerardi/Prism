9f2cf8a0007d332fd7b5dbe589436b03ae9f4cd9
Tyler Bainbridge <tyler_bainbridge@student.uml.edu>
2016-03-09 22:59:58 -0500

    fixed bugs

--- public/js/notification-page.js
+++ public/js/notification-page.js
@@ -6,7 +6,7 @@ $( window ).load( function(){
     var notifications = "";
 
     if (typeof notifuser.notifications != 'undefined') {
-        for(var i=0; i < notifuser.notifications.length; i++) {
+        for(var i=notifuser.notifications.length-1; i >= 0; i--) {
             console.log(notifuser.notifications[i].content);
             notifications += "<a href=\"" + notifuser.notifications[i].link + "\">" +
             "<p>" + notifuser.notifications[i].content + "</p></a>";
